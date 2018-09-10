9be14b9792f22bc6127833ae77380c89aaaad061
Leonardo Matos <leomp120894@gmail.com>
2018-09-10 14:31:43 -0300

    concat main and partials and compress end file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const gulp = require('gulp')
 const browserSync = require('browser-sync').create()
 const reload = browserSync.reload
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify')
+const pump = require('pump')
+const gulpSequence = require('gulp-sequence')
+const rename = require('gulp-rename')
 
 // watch JS and html files
 gulp.task('serve', function () {
@@ -21,3 +26,25 @@ gulp.task('serve', function () {
   })
   gulp.watch([ './sample/*.html', './main.js' ]).on('change', reload)
 })
+
+gulp.task('concat', function () {
+  // concat main and partials
+  return gulp.src([
+    './partials/refract-query/src/browser-vanilla.js',
+    './main.js'
+  ])
+  .pipe(concat('refapp.js'))
+  .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('compress', function (cb) {
+  // compress file
+  pump([
+    gulp.src('./dist/refapp.js'),
+    uglify(),
+    rename({ suffix: '.min' }),
+    gulp.dest('./dist/')
+  ], cb)
+})
+
+gulp.task('dist', gulpSequence('concat', 'compress'))
