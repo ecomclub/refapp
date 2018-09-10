fd5fb2c5d2e30ae2efe21779af3404f35b860668
Leonardo Matos <leomp120894@gmail.com>
2018-09-10 18:43:10 -0300

    list resource groups

--- dist/refapp.js
+++ dist/refapp.js
@@ -108,21 +108,27 @@ elementQuery = { element: 'category', 'meta': { 'classes': 'api' } }
 
   // setup as jQuery plugin
   $.fn.refapp = function (refract, options) {
+    var i
+    if (!options) {
+      // default empty options object
+      options = {}
+    }
+
     // create DOM elements
     // main app Components
-    var $aside = $('<aside />', {
+    var $aside = $('<aside>', {
       'class': ''
     })
 
     // compose Reference App layout
-    var $sidebar = $('<div />', {
+    var $sidebar = $('<div>', {
       'class': 'ref-sidebar col-md-3 col-xl-2 bg-light p-3',
       html: $aside
     })
-    var $article = $('<div />', {
+    var $article = $('<div>', {
       'class': 'ref-article col-md-4 col-xl-5 bg-white py-3 px-5'
     })
-    var $console = $('<div />', {
+    var $console = $('<div>', {
       'class': 'ref-console col-md-5 bg-dark py-3 px-5'
     })
 
@@ -135,17 +141,40 @@ elementQuery = { element: 'category', 'meta': { 'classes': 'api' } }
     https://github.com/apiaryio/drafter
     https://api-elements.readthedocs.io/en/latest/
     */
-    var element, elementQuery
-    // API title
-    elementQuery = { element: 'category', meta: { classes: 'api' } }
-    element = refractQuery(refract, elementQuery)[0]
-    $aside.append($('<h4 />', {
-      text: element.meta.title
-    }))
+    var elements, elementQuery
 
-    this.append($('<div />', {
+    // get API title
+    elementQuery = { element: 'category', meta: { classes: 'api' } }
+    elements = refractQuery(refract, elementQuery)
+    if (elements.length) {
+      $aside.append($('<h4>', {
+        text: elements[0].meta.title
+      }))
+    }
+
+    // list resource groups
+    elementQuery = { element: 'category', meta: { classes: 'resourceGroup' } }
+    elements = refractQuery(refract, elementQuery)
+    if (elements.length) {
+      var $li = []
+      for (i = 0; i < elements.length; i++) {
+        $li.push($('<li>', {
+          html: $('<a>', {
+            href: '#',
+            text: elements[i].meta.title
+          })
+        }))
+      }
+      // add list element to sidebar
+      $sidebar.append($('<ul>', {
+        'class': 'list-unstyled',
+        html: $li
+      }))
+    }
+
+    this.append($('<div>', {
       'class': 'container-fluid',
-      html: $('<div />', {
+      html: $('<div>', {
         'class': 'row',
         html: [ $sidebar, $article, $console ]
       })
