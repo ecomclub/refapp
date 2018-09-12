2808bb684df8529a0e1ddfc08abe3e317613dceb
Leonardo Matos <leomp120894@gmail.com>
2018-09-12 18:11:31 -0300

    rendering API endpoint bar

--- partials/api-console.js
+++ partials/api-console.js
@@ -7,16 +7,65 @@
    'use strict'
 
    var platform = function () {
+     // create main DOM element
+     var $inputUrl = $('<input>', {
+       'class': 'form-control',
+       type: 'search',
+       placeholder: 'Enter request URL'
+     })
+
      // returns DOM element
-     return $('<div>', {
-       'class': 'container-fluid d-none',
-       html: $('<div>', {
-         'class': 'row',
-         html: [
-           $('<header>', {
+     return $('<section>', {
+       html: [
+         // API endpoint bar
+         $('<header>', {
+           'class': 'navbar navbar-light bg-light',
+           html: $('<form>', {
+             'class': 'container',
+             action: 'javascript:;',
+             html: $('<div>', {
+               'class': 'input-group',
+               html: [
+                 $inputUrl,
+                 $('<div>', {
+                   'class': 'input-group-append',
+                   html: [
+                     $('<button>', {
+                       'class': 'btn btn-outline-secondary',
+                       type: 'button',
+                       text: 'Params'
+                     }),
+                     $('<select>', {
+                       'class': 'rounded-0 custom-select',
+                       html: [
+                         $('<option>', {
+                           value: 'GET',
+                           text: 'GET',
+                           selected: true
+                         })
+                       ]
+                     }),
+                     $('<button>', {
+                       'class': 'btn btn-primary',
+                       type: 'submit',
+                       text: 'Send'
+                     })
+                   ]
+                 })
+               ]
+             })
            })
-         ]
-       })
+         }),
+
+         // App body
+         $('<div>', {
+           'class': 'container',
+           html: $('<div>', {
+             'class': 'row',
+             html: []
+           })
+         })
+       ]
      })
    }
 
